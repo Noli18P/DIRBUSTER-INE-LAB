19801f4432245c2970853b2ab4a733c0318e76ec
Martin Martinez <61601131+Noli18P@users.noreply.github.com>
2021-07-27 18:48:40 -0700

    Reports and files found

---
+++ developer-note-readme.txt
@@ -0,0 +1,6 @@
+@Dev Team: ALWAYS make a backup copy of any file in production.
+
+Copy it by changing its extension to old, bak, xxx or whatever you want, but do not blindly overwrite a file!
+
+Thanks
+ Your project manager
---
+++ signup.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by Free CSS Templates
+http://www.freecsstemplates.org
+Released for free under a Creative Commons Attribution 2.5 License
+
+Title      : Travel Portal
+Version    : 1.0
+Released   : 20070618
+Description: A two-column, fixed-width template.
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>AwdMgmt</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="default.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="page">
+	<div id="sidebar">
+		<div id="logo">
+			<h1><a href="#">AwdMgmt</a></h1>
+		</div>
+		<!-- end header -->
+		<div id="menu">
+			<ul>
+                
+
+<li><a href="index.php">Home</a></li><li><a href="news.php">News</a></li><li><a href="awards.php">Awards</a></li><li><a href="offline.php">Sing up</a></li>
+
+			</ul>
+		</div>
+		<!-- end menu -->
+		<div id="login">
+			<h2 class="title1">Login</h2>
+			<form id="form1" method="post" action="login.php">
+				<fieldset>
+				<label for="inputtext1">Email:</label>
+				<input id="inputtext1" type="text" name="username" value="" autocomplete="off"/>
+				<label for="inputtext2">Password:</label>
+				<input id="inputtext2" type="password" name="password" value="" />
+				<input id="inputsubmit1" type="submit" name="submit" value="Login" />
+				<p><a href="#">Forgot your password?</a><br />
+				</fieldset>
+			</form>
+		</div>
+	</div>
+	<!-- end sidebar -->
+	<div id="content">
+		<div><img src="images/img08.jpg" alt="" width="740" height="220" /></div>
+		<div class="boxed">
+			<h1 class="title2">Sign-up</h1>
+			<p>TODO: Sign-up page</p>
+            <pre>
+@Dev team: the DB credentials are
+    Username:   awdmgmt
+    Password:   UChxKQk96dVtM07
+    Host:       10.104.11.198
+    DB:         awdmgmt_accounts
+    DMBS:       MySQL
+
+To test the credentials you can use mysql via command line (Windows, Linux, Mac):
+    example: mysql -u USERNAME -pPASSWORD -h HOST DB
+
+Best regards
+    IT TEAM
+
+            </pre>
+        </div>
+	    </div>
+	<!-- end content -->
+	<div style="clear: both;">&nbsp;</div>
+</div>
+<!-- end page -->
+<div id="footer">
+	<p id="legal">Copyright &copy; 2021 AwdMgmt.</p>
+	<p id="links"><a href="#">Privacy terms</a> | <a href="#">Licensing</a></p>
+</div>
+</body>
+</html>
